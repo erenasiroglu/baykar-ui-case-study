ce8f69c5b6761513bed23c5f1fb8aa08882af6ea
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2025-01-25 14:02:01 +0300

    refactor: update button structure and layout in SectionOne component

--- src/app/components/SectionOne.tsx
+++ src/app/components/SectionOne.tsx
@@ -23,21 +23,23 @@ const Header = () => {
       </div>
 
       <div className="flex items-center justify-center flex-grow">
-        <div className="flex items-center bg-customPurple text-white p-2 rounded-full">
+        <button className="flex items-center bg-customPurple text-white p-2 rounded-full">
           <Image
             src="/icons/app-logo.svg"
             alt="app-logo"
             width={24}
             height={24}
           />
-        </div>
+        </button>
         <span className="text-customTitle font-semibold ml-2">Logo</span>
       </div>
 
-      <div className="flex items-center space-x-4 ml-auto">
-        <Image src="/icons/user.svg" alt="user" width={18} height={18} />
-        <span className="text-customTitle font-normal">Login/Register</span>
-        <button className="bg-customPurple text-white p-2 rounded-full w-[169px] flex items-center justify-center gap-4">
+      <div className="flex items-center gap-4">
+        <button className="flex items-center gap-2 flex-row">
+          <Image src="/icons/user.svg" alt="user" width={18} height={18} />
+          <span className="text-customTitle font-normal">Login/Register</span>
+        </button>
+        <button className="bg-customPurple text-white w-[169px] h-[50px] rounded-full w-[169px] flex items-center justify-center gap-2">
           <Image src="/icons/app-logo.svg" alt="user" width={20} height={20} />
           <span>Add Listing</span>
         </button>
